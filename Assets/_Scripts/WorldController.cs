169df716f24ab1fc6ce98614e14bcc108227731e
Robert-Anthony-Di-Monaco <rob75dimonaco@gmail.com>
2016-04-20 22:47:07 -0400

    Map additions

---
+++ Assets/_Scripts/WorldController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldController : MonoBehaviour
+{
+    // World event objects
+    public GameObject Lake, 
+                      elevator1, elevator2;
+
+	void Start ()
+    {
+	    
+	}
+	
+	void Update ()
+    {
+	    
+	}
+}
