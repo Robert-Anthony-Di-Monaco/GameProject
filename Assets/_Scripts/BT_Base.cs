a8f7f13a1dc98a18980e97468e712d26e973fa23
Robert-Anthony-Di-Monaco <rob75dimonaco@gmail.com>
2016-04-22 21:26:52 -0400

    Map fixes + script set up

---
+++ Assets/_Scripts/BT_Base.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using Random = UnityEngine.Random;
+using Coroutine = System.Collections.IEnumerator;
+using BTCoroutine = System.Collections.Generic.IEnumerator<BTNodeResult>;
+
+public class BT_Base : MonoBehaviour
+{
+    public Transform NavMeshTarget;
+
+    private BehaviorTree bt;
+
+    void Awake()
+    {
+        InitBT();
+        bt.Start();
+    }
+
+    void Update()
+    {
+    }
+
+    private void InitBT()
+    {
+        bt = new BehaviorTree(Application.dataPath + "/_Scripts/BT_Tree_Example.xml", this);
+    }
+
+    // Condition
+    [BTLeaf("")]
+    public bool TooManyChasers()
+    {
+        return false;
+    }
+    
+    // Leaf
+    [BTLeaf("")]
+    public BTCoroutine Chase()
+    {
+       yield return BTNodeResult.NotFinished;
+    }
+}
+
+
+
--- Assets/_Scripts/WorldController.cs
+++ Assets/_Scripts/WorldController.cs
@@ -12,8 +12,8 @@ public class WorldController : MonoBehaviour
 {
     // World event objects
     public GameObject Lake,
-                      playerElevator, enemyElevator,
-                      leftElevator, rightElevator;
+                      playerElevator, enemyElevator,   // 275.4 ---> 26.1  for enemy, 97.45 for player
+                      leftElevator, rightElevator;     // 251 --> 0.1
 
 	void Start ()
     {
@@ -22,6 +22,27 @@ public class WorldController : MonoBehaviour
 	
 	void Update ()
     {
-	    
+        //if(button pressed)
+            //moveElevator(leftElevator)
 	}
+
+    /* Raise or Lower elevator when activated
+     *      parameter: which elevator to move     
+    */ void moveElevator(GameObject elevator)
+    {
+                
+    }
+
+    // Activate Volcano eruption
+    void erupt()
+    {
+
+    }
+
+    /* Moves the Lake up or down 
+     *      parameter: direction the lake moves in 
+    */ void moveLake(string direction)
+    {
+
+    }
 }
