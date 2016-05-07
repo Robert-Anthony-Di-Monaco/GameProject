bf66d887798fff8a7d76cb5ac3dc7176c2adf1a0
Robert-Anthony-Di-Monaco <rob75dimonaco@gmail.com>
2016-05-07 17:27:24 -0400

    Camera Fix

--- Assets/zzTESTING/TestingCamera.cs
+++ Assets/zzTESTING/TestingCamera.cs
@@ -3,14 +3,15 @@ using System.Collections;
 
 public class TestingCamera : MonoBehaviour
 {
-    public Transform TargetAnchor; 
-    float rotX = 0, rotY = 0;
-    float sensitivity = 5f;
-
+    public Transform TargetAnchor;
+    public float sensitivity;
+    float rotX, rotY;
+    
 	void Start ()
     {
-       
-	}
+        rotX = Input.GetAxis("Mouse Y");
+        rotY = Input.GetAxis("Mouse X");
+    }
 	
 	void Update ()
     {
@@ -19,24 +20,28 @@ public class TestingCamera : MonoBehaviour
         // Only rotate the camera if Mouse-Right-Click is held down
         if (Input.GetMouseButton(1))
         {
-            rotX -= Input.GetAxis("Mouse Y") * sensitivity;
-            rotY += Input.GetAxis("Mouse X") * sensitivity;
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(rotX, rotY, 0), sensitivity);
+            rotX -= Input.GetAxis("Mouse Y");
+            rotY += Input.GetAxis("Mouse X");
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(rotX, rotY, 0), sensitivity * Time.deltaTime);
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            ResetCamera();
         }
     }
-    void LateUpdate()
+    void FixedUpdate()
     {
         if (Physics.Linecast(transform.position, TargetAnchor.parent.position))
         {
-            transform.position -= new Vector3(0, 10f, 0);
+
         }
     }
 
     // Reset camera back to default orientation
-    void resetCamera()
+    void ResetCamera()
     {
-        //transform.position = new Vector3(TargetAnchor.position.x, TargetAnchor.position.y + 6f, TargetAnchor.position.z - 6f);
         transform.rotation = Quaternion.Euler(20f, 0, 0);
+        transform.forward = (TargetAnchor.position - transform.position).normalized;
     }
 }
 
@@ -53,7 +58,4 @@ public class TestingCamera : MonoBehaviour
 /*
     Slowly rotate to go to face so can see it from the another view, 
     Also to go under stuff
-    Also for turning camera make it slower  ---> JUST ALTER SENETIVITY VAR UP TOP
-     
-     
      */
