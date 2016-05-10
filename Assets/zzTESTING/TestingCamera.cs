d7d25c75a14056cb1c13209bcb6e96ad3fdd262f
Robert-Anthony-Di-Monaco <rob75dimonaco@gmail.com>
2016-05-10 16:20:12 -0400

    Camera change

--- Assets/zzTESTING/TestingCamera.cs
+++ Assets/zzTESTING/TestingCamera.cs
@@ -4,99 +4,37 @@ using System.Collections;
 public class TestingCamera : MonoBehaviour
 {
     public Transform TargetAnchor;
-    public float sensitivity;
-    float rotX, rotY;
-    float centerX, centerY;
-    float lastMouseX, lastMouseY;
-    Vector3 newForward = Vector3.zero;  
+    float rotX = 0, rotY = 0;
+    float sincePressedOnce = 0;
+    bool pressedOnce = false;
     
-	void Start ()
-    {
-        rotX = Input.GetAxis("Mouse Y");
-        rotY = Input.GetAxis("Mouse X");
-
-        //centerX = Screen.width / 2;
-        //centerY = Screen.height / 2;
-        lastMouseX = Input.mousePosition.x;
-        lastMouseY = Input.mousePosition.y;
-    }
-
-    void OnGUI()
-    {
-        Event e = Event.current;
-        if (e.isMouse)
-        {
-            //lastMouseX = Input.mousePosition.x;
-            //lastMouseY = Input.mousePosition.y;
-
-            float targetX = lastMouseX + e.delta.magnitude * e.delta.normalized.x;
-            float targetY = lastMouseY + e.delta.magnitude * e.delta.normalized.y;
-            newForward = (new Vector3(targetX, targetY, 0) - transform.position).normalized;
-            //transform.rotation = Quaternion.FromToRotation(transform.forward, newForward);
-
-            if (e.mousePosition.x > centerX)
-            {
-                rotY -= 5f;
-            }
-            else if (e.mousePosition.x < centerX)
-            {
-                rotY += 5f;
-            }
-
-            if (e.mousePosition.y > centerY)
-            {
-                rotX += 5f;
-            }
-            else if (e.mousePosition.y < centerY)
-            {
-                rotX -= 5f;
-            }
-        }
-
-        //centerX = Input.mousePosition.x;
-        //centerY = Input.mousePosition.y;
-    }
-
-
     void Update ()
     {
-        lastMouseX = Input.mousePosition.x;
-        lastMouseY = Input.mousePosition.y;
         transform.position = TargetAnchor.position;
 
-        // Only rotate the camera if Mouse-Right-Click is held down
-        if (Input.GetMouseButton(1))
-        {
-            //rotY += Input.GetAxis("Mouse X");
-            //rotX -= Input.GetAxis("Mouse Y");
-            //rotX = Mathf.Clamp(rotX, -35f, 75f);
-
-            //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(rotX, rotY, 0), 0.05f);
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.FromToRotation(transform.forward, newForward), 0.05f);
-            
-        }
+        // If user double clicks Right-Click reset the camera
+        pressedOnce = ( Input.GetMouseButtonDown(1) && (Time.time - sincePressedOnce) < 1.5f ) ? true : false;
         if (Input.GetMouseButtonDown(1))
         {
-            //ResetCamera();
-        }
-    }
-    void FixedUpdate()
-    {
-        if (Physics.Linecast(transform.position, TargetAnchor.parent.position))
+            if (pressedOnce)
+            {
+                transform.forward = (TargetAnchor.position - transform.position).normalized;
+                transform.rotation = Quaternion.Euler(25f, 0, 0);
+            }
+            else
+                sincePressedOnce = Time.time;
+        }   
+
+        // If user holds down Right-Click rotate camera
+        if (Input.GetMouseButton(1))
         {
+            rotY += Input.GetAxis("Mouse X") * 3.5f;
+            rotX -= Input.GetAxis("Mouse Y") * 2.5f;
+            rotX = Mathf.Clamp(rotX, -35f, 60f);
 
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(rotX, rotY, 0), 0.05f);
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0);
         }
-    }
-    void LateUpdate()
-    {
-       
-    }
-
-    // Reset camera back to default orientation
-    void ResetCamera()
-    {
-        transform.rotation = Quaternion.Euler(20f, 0, 0);
-        transform.forward = (TargetAnchor.position - transform.position).normalized;
     }
 }
 
@@ -104,13 +42,3 @@ public class TestingCamera : MonoBehaviour
 /*  
  *  Camera moves to other point quickly see all inbetween  
  */
-
-    
-
-
-
-
-/*
-    Slowly rotate to go to face so can see it from the another view, 
-    Also to go under stuff
-     */
