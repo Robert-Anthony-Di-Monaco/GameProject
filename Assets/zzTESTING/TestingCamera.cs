3e455d24eb714f3349a91e4e6f64991c9f1963a2
Robert-Anthony-Di-Monaco <rob75dimonaco@gmail.com>
2016-05-09 23:04:01 -0400

    Camera

--- Assets/zzTESTING/TestingCamera.cs
+++ Assets/zzTESTING/TestingCamera.cs
@@ -6,27 +6,78 @@ public class TestingCamera : MonoBehaviour
     public Transform TargetAnchor;
     public float sensitivity;
     float rotX, rotY;
+    float centerX, centerY;
+    float lastMouseX, lastMouseY;
+    Vector3 newForward = Vector3.zero;  
     
 	void Start ()
     {
         rotX = Input.GetAxis("Mouse Y");
         rotY = Input.GetAxis("Mouse X");
+
+        //centerX = Screen.width / 2;
+        //centerY = Screen.height / 2;
+        lastMouseX = Input.mousePosition.x;
+        lastMouseY = Input.mousePosition.y;
     }
-	
-	void Update ()
+
+    void OnGUI()
     {
+        Event e = Event.current;
+        if (e.isMouse)
+        {
+            //lastMouseX = Input.mousePosition.x;
+            //lastMouseY = Input.mousePosition.y;
+
+            float targetX = lastMouseX + e.delta.magnitude * e.delta.normalized.x;
+            float targetY = lastMouseY + e.delta.magnitude * e.delta.normalized.y;
+            newForward = (new Vector3(targetX, targetY, 0) - transform.position).normalized;
+            //transform.rotation = Quaternion.FromToRotation(transform.forward, newForward);
+
+            if (e.mousePosition.x > centerX)
+            {
+                rotY -= 5f;
+            }
+            else if (e.mousePosition.x < centerX)
+            {
+                rotY += 5f;
+            }
+
+            if (e.mousePosition.y > centerY)
+            {
+                rotX += 5f;
+            }
+            else if (e.mousePosition.y < centerY)
+            {
+                rotX -= 5f;
+            }
+        }
+
+        //centerX = Input.mousePosition.x;
+        //centerY = Input.mousePosition.y;
+    }
+
+
+    void Update ()
+    {
+        lastMouseX = Input.mousePosition.x;
+        lastMouseY = Input.mousePosition.y;
         transform.position = TargetAnchor.position;
 
         // Only rotate the camera if Mouse-Right-Click is held down
         if (Input.GetMouseButton(1))
         {
-            rotX -= Input.GetAxis("Mouse Y");
-            rotY += Input.GetAxis("Mouse X");
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(rotX, rotY, 0), sensitivity * Time.deltaTime);
+            //rotY += Input.GetAxis("Mouse X");
+            //rotX -= Input.GetAxis("Mouse Y");
+            //rotX = Mathf.Clamp(rotX, -35f, 75f);
+
+            //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(rotX, rotY, 0), 0.05f);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.FromToRotation(transform.forward, newForward), 0.05f);
+            
         }
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(1))
         {
-            ResetCamera();
+            //ResetCamera();
         }
     }
     void FixedUpdate()
@@ -35,6 +86,10 @@ public class TestingCamera : MonoBehaviour
         {
 
         }
+    }
+    void LateUpdate()
+    {
+       
     }
 
     // Reset camera back to default orientation
@@ -50,7 +105,7 @@ public class TestingCamera : MonoBehaviour
  *  Camera moves to other point quickly see all inbetween  
  */
 
-    // Rigidbody/Agent use velocity to position camera behind
+    
 
 
 
