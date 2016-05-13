81259ebd50a19c138b48b6fb1ae8c79646d9afc7
Robert-Anthony-Di-Monaco <rob75dimonaco@gmail.com>
2016-05-12 20:06:08 -0400

    Small fix

--- Assets/zzTESTING/TestingCamera.cs
+++ Assets/zzTESTING/TestingCamera.cs
@@ -5,25 +5,10 @@ public class TestingCamera : MonoBehaviour
 {
     public Transform TargetAnchor, AlternateAnchor;
     float rotX = 0, rotY = 0;
-    float sincePressedOnce = 0;
-    bool pressedOnce = false;
     
     void Update ()
     {
         transform.position = TargetAnchor.position;
-
-        // If user double clicks Right-Click reset the camera
-        pressedOnce = ( Input.GetMouseButtonDown(1) && (Time.time - sincePressedOnce) < 1.5f ) ? true : false;
-        if (Input.GetMouseButtonDown(1))
-        {
-            if (pressedOnce)
-            {
-                transform.forward = (TargetAnchor.position - transform.position).normalized;
-                transform.rotation = Quaternion.Euler(25f, 0, 0);
-            }
-            else
-                sincePressedOnce = Time.time;
-        }   
 
         // If user holds down Right-Click rotate camera
         if (Input.GetMouseButton(1))
@@ -34,6 +19,41 @@ public class TestingCamera : MonoBehaviour
 
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(rotX, rotY, 0), 0.05f);
             transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0);
+        }
+        else   // Otherwise rotate back to face target
+        {
+            Vector3 targetFwd = (TargetAnchor.parent.position - transform.position).normalized;
+            transform.forward = Vector3.Slerp(transform.forward, targetFwd, 0.025f);
+        }
+        
+        // 
+        if (Input.GetKey(KeyCode.Q))
+        {
+
+        }
+
+        // 
+        if (Input.GetKey(KeyCode.W))
+        {
+
+        }
+
+        // 
+        if (Input.GetKey(KeyCode.E))
+        {
+
+        }
+
+        // 
+        if (Input.GetKey(KeyCode.R))
+        {
+
+        }
+
+        // 
+        if (Input.GetKey(KeyCode.Space))
+        {
+
         }
     }
 
