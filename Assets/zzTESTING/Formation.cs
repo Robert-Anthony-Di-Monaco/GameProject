f83f9eaa7c370fc658aba7cda095a82eb3d22f61
Robert-Anthony-Di-Monaco <rob75dimonaco@gmail.com>
2016-05-14 21:37:46 -0400

    Alternate for vanguard and early formation

---
+++ Assets/zzTESTING/Formation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Formation : MonoBehaviour
+{
+    // type Unit
+    // List<Unit> Platoon; 
+    // Lists : AssaultInfiltry, ShieldVanguard, ExplosiveInfiltry, ReconAssassin, MobileArtillery, 
+    // SuperSoldier, MutatedSoldier -> can lose control rage if too much damage will attack all near
+
+	void Start ()
+    {
+	    
+	}
+	
+	void Update ()
+    {
+	
+	}
+}
+
+// kill leader all disperse, stelth is vauable or spy pretend be them or plant bombs on them. detonate wtich to it the stealth climber
