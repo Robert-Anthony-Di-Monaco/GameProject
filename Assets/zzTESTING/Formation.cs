70d01f6c0f848c7f56cea9db20f4e813ef9aafad
Robert-Anthony-Di-Monaco <rob75dimonaco@gmail.com>
2016-05-20 07:13:50 -0400

    How to do formations

--- Assets/zzTESTING/Formation.cs
+++ Assets/zzTESTING/Formation.cs
@@ -1,26 +1,33 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class Formation : MonoBehaviour
-{
-    // type Unit
-    // List<Unit> Platoon; 
-    // Lists : AssaultInfiltry, ShieldVanguard, ExplosiveInfiltry, ReconAssassin, MobileArtillery, 
+{ 
     // SuperSoldier, MutatedSoldier -> can lose control rage if too much damage will attack all near
 
     /*
-            Center anchor is ball --> dont render
-
             1 shield + 2 guns rockets
             4 shields or tank?
             8 guns
             4 rockets
             2 artilery --> back up stuff, little robots to attack others, can drop them
             
-
-            BUTTON to equal up 2 platoons
+            BUTTON to equal up 2 diff platoons
             BUTTON for each type to attack, or to send it out only artillery
      */
+    
+
+
+    // Slots for each type, get 1st available in list, arranged by closest to center, parented slot positions 
+    // Alter how not moving and  no tank or seiege so circle formation
+    
+    List<Unit> ShieldVanguard_Slots,    // 1st line in formation
+               AssaultInfantry_Slots,   // 2nd line
+               ExplosiveInfantry_Slots, // 3rd line
+               ArmoredProtector_Slots,  // Can move between front and back
+               MobileArtillery_Slots,   // Can move between front and back
+               AssassinSpy_Slots;       // On their own
+
 
 	void Start ()
     {
@@ -33,7 +40,6 @@ public class Formation : MonoBehaviour
 	}
 }
 
-// kill leader all disperse, stelth is vauable or spy pretend be them or plant bombs on them. detonate wtich to it the stealth climber
-/*
-    start from its local space its position calcualte a free slot, have a list of free spots?     
-*/
+// kill leader all disperse, stelth is valuable or spy pretend be them or plant bombs on them. detonate wtich to it the stealth climber
+// Moving in line, Stop do arc
+// when not moving and no tank then form circle aim all over 
